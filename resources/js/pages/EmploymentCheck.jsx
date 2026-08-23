4a1bb7c92383f343e70670099bf215efccd3da56
parashpandey <pandeyparas1234@gmail.com>
2026-08-23 13:24:18 +0530

    message

--- resources/js/pages/EmploymentCheck.jsx
+++ resources/js/pages/EmploymentCheck.jsx
@@ -42,6 +42,9 @@ export default function EmploymentCheck() {
   };
 
   return (
+    <>
+        <Sidebar />
+    <Header />
     <div style={styles.container}>
       <form style={styles.card}>
         <h2 style={styles.title}>Database Check Verification</h2>
@@ -151,6 +154,8 @@ export default function EmploymentCheck() {
         </div>
       </form>
     </div>
+    </>
+    
   );
 }
 
