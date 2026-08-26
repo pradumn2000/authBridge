bff404f2fa0349a0d373fd10d9f453ce49ce43ee
parashpandey <pandeyparas1234@gmail.com>
2026-08-26 22:54:54 +0530

    Add new Product

--- resources/js/pages/Sidebar.jsx
+++ resources/js/pages/Sidebar.jsx
@@ -981,7 +981,7 @@ const CHECK_TYPE_SUBMENU = [
   { path: "/CriminalCheck", label: "Criminal" },
   { path: "/DrugtestCheck", label: "Drug Test" },
   { path: "/CourtroomCheck", label: "Courtroom" },
-  { path: "/AddCheckType/New", label: "Product Add" },
+  { path: "/AddCheckType/New", label: "Add NewProduct" },
 ];
 
 // Client Management Submenu
