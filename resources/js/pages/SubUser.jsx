287ff760acec0ae7fc3d0f80d515ae3f323e9473
parashpandey <pandeyparas1234@gmail.com>
2026-09-23 00:23:54 +0530

    message

--- resources/js/pages/SubUser.jsx
+++ resources/js/pages/SubUser.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { API_URL } from "../src/config";
 import { 
   FiSearch, 
   FiDownload, 
